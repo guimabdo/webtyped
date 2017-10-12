59d5fc6c7d2917b85dccabde3165ba090750c644
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2017-10-12 20:49:31 -0300

    removing old proj

--- test/WebTyped.Example.Web_old/Scripts/main.ts
+++
@@ -1,5 +0,0 @@
-﻿import { Http } from '@angular/http';
-import { WebApiEventEmmiterService } from '@guimabdo/webtyped-angular';
-import { ValuesService } from './webApi/values.service';
-var vs = new ValuesService(null, null);
-alert('oi');
