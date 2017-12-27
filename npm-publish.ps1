27cc8cd0b2a5841d88add1bfdb5a743b83e12515
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2017-12-27 18:51:55 -0200

    npmpub

--- npm-publish.ps1
+++ npm-publish.ps1
@@ -1,6 +1,7 @@
-"//registry.npmjs.org/:_authToken=`$`{npm_token`}" | Out-File (Join-Path $ENV:APPVEYOR_BUILD_FOLDER ".npmrc") -Encoding UTF8
 $version = $env:APPVEYOR_BUILD_VERSION;
 $vMessage = "version: $version"
+$token = $env:npm_token
+"//registry.npmjs.org/:_authToken=$token" | Out-File (Join-Path $ENV:APPVEYOR_BUILD_FOLDER ".npmrc") -Encoding UTF8
 Write-Host $vMessage
 Write-Host "showing npm v"
 npm -v
