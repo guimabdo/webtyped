bbbb2f0705754dfe3bd88774f788ea4e2d6c68f1
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2017-12-27 18:10:18 -0200

    trying publish npm5

--- npm-publish.ps1
+++ npm-publish.ps1
@@ -1,17 +1,18 @@
-"//registry.npmjs.org/:_authToken=$($ENV:npm_token)" | Out-File (Join-Path $ENV:APPVEYOR_BUILD_FOLDER ".npmrc") -Encoding UTF8
+"//registry.npmjs.org/:_authToken=`$`{npm_token`}" | Out-File (Join-Path $ENV:APPVEYOR_BUILD_FOLDER ".npmrc") -Encoding UTF8
+$vMessage = "version: $env:version"
+Write-Host $vMessage
 set-location ./src/WebTyped.Npm/common
-Write-Host 'version: $($ENV:version)'
-npm version $ENV:version -m 'v$($ENV:version)'
+npm version $env:version -m 'v$($ENV:version)'
 npm publish
 set-location ../fetch
-npm version $ENV:version -m 'v$($ENV:version)'
+npm version $env:version -m 'v$($ENV:version)'
 npm publish
 set-location ../jquery
-npm version $ENV:version -m 'v$($ENV:version)'
+npm version $env:version -m 'v$($ENV:version)'
 npm publish
 set-location ../angular
-npm version $ENV:version -m 'v$($ENV:version)'
+npm version $env:version -m 'v$($ENV:version)'
 npm publish
 set-location ../common
-npm version $ENV:version -m 'v$($ENV:version)'
+npm version $env:version -m 'v$($ENV:version)'
 npm publish
