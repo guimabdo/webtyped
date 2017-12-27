b1fa3529744674cef126a3b1e4f2670651aef88b
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2017-12-27 18:27:30 -0200

    npmpub2

--- npm-publish.ps1
+++ npm-publish.ps1
@@ -1,18 +1,19 @@
 "//registry.npmjs.org/:_authToken=`$`{npm_token`}" | Out-File (Join-Path $ENV:APPVEYOR_BUILD_FOLDER ".npmrc") -Encoding UTF8
-$vMessage = "version: $env:APPVEYOR_BUILD_VERSION"
+$version = $env:APPVEYOR_BUILD_VERSION;
+$vMessage = "version: $version"
 Write-Host $vMessage
 set-location ./src/WebTyped.Npm/common
-npm version $env:version -m $vMessage
+npm version $version -m $vMessage
 npm publish
 set-location ../fetch
-npm version $env:version -m $vMessage
+npm version $version -m $vMessage
 npm publish
 set-location ../jquery
-npm version $env:version -m $vMessage
+npm version $version -m $vMessage
 npm publish
 set-location ../angular
-npm version $env:version -m $vMessage
+npm version $version -m $vMessage
 npm publish
 set-location ../common
-npm version $env:version -m $vMessage
+npm version $version -m $vMessage
 npm publish
