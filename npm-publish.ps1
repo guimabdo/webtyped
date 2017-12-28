8ed8479714e6965e71502d16c95ea246da752237
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2017-12-27 22:04:30 -0200

    mixed solution?

--- npm-publish.ps1
+++ npm-publish.ps1
@@ -2,19 +2,3 @@ $version = $env:APPVEYOR_BUILD_VERSION;
 $vMessage = "version: $version"
 $token = $env:npm_token
 "//registry.npmjs.org/:_authToken=$token" | Out-File (Join-Path $ENV:APPVEYOR_BUILD_FOLDER ".npmrc") -Encoding UTF8
-npm whoami
-set-location ./src/WebTyped.Npm/common
-npm version $version -m $vMessage
-iex "npm publish"
-set-location ../fetch
-npm version $version -m $vMessage
-npm publish
-set-location ../jquery
-npm version $version -m $vMessage
-npm publish
-set-location ../angular
-npm version $version -m $vMessage
-npm publish
-set-location ../common
-npm version $version -m $vMessage
-npm publish
