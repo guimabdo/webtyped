25789c99c5c42295b13b773c17e02cb39d106db5
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2017-12-27 22:29:21 -0200

    publish all packages to npm

--- npm-publish.ps1
+++
@@ -1,4 +0,0 @@
-$version = $env:APPVEYOR_BUILD_VERSION;
-$vMessage = "version: $version"
-$token = $env:npm_token
-"//registry.npmjs.org/:_authToken=$token" | Out-File (Join-Path $ENV:APPVEYOR_BUILD_FOLDER ".npmrc") -Encoding UTF8
