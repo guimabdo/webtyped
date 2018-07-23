7f12c05619e22355bb940c6cb726f3009cfde05c
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2018-07-23 15:25:57 -0300

    optional members for inference trick...

--- src/WebTyped.Npm/common/webTypedFunction.ts
+++ src/WebTyped.Npm/common/webTypedFunction.ts
@@ -1,6 +1,6 @@
 ﻿export interface WebTypedFunction<TParameters, TResult> {
 	//Did not get it => https://github.com/Microsoft/TypeScript/wiki/FAQ#why-doesnt-type-inference-work-on-this-interface-interface-foot---
 	//Adding members for making inference work on inherited types
-	_p: TParameters;
-	_r: TResult;
+	_p?: TParameters;
+	_r?: TResult;
 }
