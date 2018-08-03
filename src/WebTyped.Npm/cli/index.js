ce8787169697060313362161f912d11d14ce6f55
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2018-08-03 18:26:36 -0300

    showing version from package.json

--- src/WebTyped.Npm/cli/index.js
+++ src/WebTyped.Npm/cli/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-
-console.log('WebTyped - v.vDev');
+var pkg = require('./package.json');
+console.log(`WebTyped - v.${pkg.version}`);
 
 var exec = require('child_process').exec;
 var cmd = `dotnet "${__dirname}/program/WebTyped.Cli.dll" ${process.argv.splice(2)}`;
