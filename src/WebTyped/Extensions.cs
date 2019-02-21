f57d0e20337db828356b8d7a456c5ac811cf7576
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2019-02-20 21:37:16 -0300

    [deploy][cli] newtonsoft camel case naming strategy

--- src/WebTyped/Extensions.cs
+++ src/WebTyped/Extensions.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace WebTyped {
 	public static class Extensions {
+		static readonly CamelCaseNamingStrategy camel = new CamelCaseNamingStrategy();
 		public static StringBuilder AppendLine(this StringBuilder sb, int level, string text) {
 			return sb
 				.Append('\t', level)
@@ -12,7 +14,8 @@ namespace WebTyped {
 
 		public static string ToCamelCase(this string str) {
 			if (string.IsNullOrWhiteSpace(str)) { return str; }
-			return str[0].ToString().ToLower() + str.Substring(1);
+			return camel.GetPropertyName(str, false);
+			//return str[0].ToString().ToLower() + str.Substring(1);
 		}
 	}
 }
