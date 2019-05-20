caa6b2041866d222f46c9a45f59d70053052fdab
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2019-05-20 20:15:54 -0300

    [deploy][classes]

--- src/WebTyped.Npm/common/webTypedCallInfo.ts
+++ src/WebTyped.Npm/common/webTypedCallInfo.ts
@@ -1,7 +1,7 @@
 ﻿import { WebTypedFunction } from "./webTypedFunction";
 export interface WebTypedCallInfo<TParameters, TResult> {
-	func: WebTypedFunction<TParameters, TResult>,
-	parameters: TParameters,
+    func: WebTypedFunction<TParameters, TResult>;
+    parameters: TParameters;
 	result?: TResult;
     kind: string;
     returnTypeName: string;
