a7b5318742ce82aa131cdba3af7c301d91175f28
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2019-05-26 15:01:05 -0300

    [deploy][classes]avoiding qs overfloew

--- src/WebTyped.Npm/common/webTypedUtils.ts
+++ src/WebTyped.Npm/common/webTypedUtils.ts
@@ -1,5 +1,5 @@
 ﻿export class WebTypedUtils {
-    private static addQueryParameterValue(path: string, val: any, result: { path: string, val: string }[], addedObjects: any[]) {
+    private static addQueryParameterValue(path: string, val: any, result: { path: string, val: string }[]) {
         //undefined dont add the parameter to the query
         if (val === undefined) { return; }
         //null add parameter with empty value
@@ -10,30 +10,29 @@
         //arrays
         if (Array.isArray(val)) {
             val.forEach((item, index) => {
-                WebTypedUtils.addQueryParameterValue(`${path}[${index}]`, item, result, addedObjects);
+                WebTypedUtils.addQueryParameterValue(`${path}[${index}]`, item, result);
             });
             return;
         }
         //complex objects
         if (typeof val === "object") {
-            WebTypedUtils.resolveQueryParameters(val, result, path, addedObjects);
+            WebTypedUtils.resolveQueryParameters(val, result, path);
             return;
         }
         //simple values
         result.push({ path, val });
     }
-    public static resolveQueryParameters(obj: any, result?: { path: string, val: string }[], parentField?: string, addedObjects?: any[]) {
+    public static resolveQueryParameters(obj: any, result?: { path: string, val: string }[], parentField?: string) {
         if (!result) { result = []; }
-        if (!addedObjects) { addedObjects = []; }
-        if (addedObjects.indexOf(obj) >= 0) { return result; }
-        addedObjects.push(obj);
+        //Avoid overflow
+        if (result.length > 1000000) { return result; }
         for (let field in obj) {
             var val = obj[field];
             var pathElements = [];
             if (parentField) { pathElements.push(parentField); }
             pathElements.push(field);
             var path = pathElements.join('.');
-            WebTypedUtils.addQueryParameterValue(path, val, result, addedObjects);
+            WebTypedUtils.addQueryParameterValue(path, val, result);
         }
         return result;
 
