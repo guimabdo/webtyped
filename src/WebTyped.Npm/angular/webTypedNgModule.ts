3c57b78d224889df78d17a4fa738c64b5c97441e
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2019-09-10 18:05:27 -0300

    [deploy][classes]

---
+++ src/WebTyped.Npm/angular/webTypedNgModule.ts
@@ -0,0 +1,18 @@
+﻿import { NgModule, ModuleWithProviders } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { WebTypedNgInvoker } from './webTypedNgInvoker';
+
+@NgModule({
+    imports:[ HttpClientModule ]
+})
+export class WebTypedNgModule { 
+    static forRoot(): ModuleWithProviders {
+		return {
+			ngModule: WebTypedNgModule,
+			providers: [
+                WebTypedEventEmitterService,
+                WebTypedNgInvoker
+			]
+		};
+	}
+}
