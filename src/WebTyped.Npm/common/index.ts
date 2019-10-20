7af9d404c9b240e3468943e7e8da9537a6031bc3
Guilherme Meinlschmiedt Abdo <guimabdo@hotmail.com>
2019-10-20 12:09:32 -0300

    removing old stuff

--- src/WebTyped.Npm/common/index.ts
+++
@@ -1,5 +0,0 @@
-﻿export * from "./webTypedEventEmitter";
-export * from "./webTypedCallInfo";
-export * from "./webTypedFunction";
-export * from "./webTypedInvoker";
-export * from "./webTypedUtils";
