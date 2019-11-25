b7a5da612de7af338ab6c5d5907dec5c0a0128c9
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2019-11-25 19:31:50 -0300

    fixing custom maps

--- test/WebTyped.Tests/ExtrernalAssemblyTest.cs
+++ test/WebTyped.Tests/ExtrernalAssemblyTest.cs
@@ -69,14 +69,68 @@ public class Test : ExternalModel {
             var output = await generator.GenerateOutputsAsync();
 		}
 
-		//string Read(string file) {
-		//	return File.ReadAllText($"../../../Inputs/{file}");
-		//}
+        [TestMethod]
+        public async Task GenerateCustomMapTest()
+        {
+            var options = new Options(".\\")
+            {
+                GenericReturnType = new ClientType
+                {
+                    Name = "Observable",
+                    Module = "rxjs"
+                },
+                CustomMap = new Dictionary<string, ClientType> {
+                    {
+                        "ExternalModel",
+                        new ClientType { Module ="out", Name = "MissingTypeFromSomewhere" }
+                    },
+                    {
+                        "ExternalModel2",
+                        new ClientType { Module ="out", Name = "MissingTypeFromSomewhere2" } 
+                    }
+                }
+            };
 
-		//async Task AssertOutput(string file) {
-		//	var cs = Read($"{file}.cs");
-		//	var output = await TestHelpers.Generate(cs);
-		//	Assert.AreEqual(Read($"{file}-output.ts"), output.First().Value);
-		//}
-	}
+            //var options = new Options(".\\", 
+            //    false, 
+            //    //ServiceMode.Angular, 
+            //    new ClientType
+            //    {
+            //        Name = "Observable",
+            //        Module = "rxjs"
+            //    },
+            //    new string[0], "", 
+            //    false, 
+            //    null,
+            //    null
+            //);
+            var generator = new Generator(
+                new string[] {
+@"
+using ExternalLib.Models;
+using ExternalLib.Models2;
+public class Test : ExternalModel {
+    public int Test2 {get;set;}
+    public ExternalModel2 Missing {get;set;}
+}
+"
+                }
+                ,
+                new string[0],
+                new Package[0],
+                new string[0],
+                options);
+            var output = await generator.GenerateOutputsAsync();
+        }
+
+        //string Read(string file) {
+        //	return File.ReadAllText($"../../../Inputs/{file}");
+        //}
+
+        //async Task AssertOutput(string file) {
+        //	var cs = Read($"{file}.cs");
+        //	var output = await TestHelpers.Generate(cs);
+        //	Assert.AreEqual(Read($"{file}-output.ts"), output.First().Value);
+        //}
+    }
 }
